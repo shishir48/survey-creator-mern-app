555b36692f085dcc473ac3174f4f4af160768fe3
shishir singh <shishirsingh@shishirs-MacBook-Air.local>
2021-05-20 04:45:44 +0530

    error fixes

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ if(process.env.NODE_ENV === 'production' ){
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
-const Port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 app.listen(port)
