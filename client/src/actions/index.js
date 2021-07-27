7a98e37ae83a7440d9051f7f9a2770ca701de449
shishir singh <shishirsingh@shishirs-MacBook-Air.local>
2021-07-27 09:02:22 +0530

    changes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { FETCH_USER } from "./types"
+import { FETCH_USER, FETCH_SURVEYS } from "./types"
 
 export const fetchUser = () => async (dispatch) => {
     const res = await axios.get("/api/current_user")
@@ -15,4 +15,9 @@ export const sendSurvey = (values, history) => async (dispatch) => {
     const res = await axios.post("/api/surveys", values)
     dispatch({ type: FETCH_USER, payload: res.data })
     history.push("/surveys")
+}
+
+export const fetchSurveys = () => async (dispatch) => {
+    const res = await axios.get('/api/surveys')
+    dispatch({type: FETCH_SURVEYS, payload:res.data})
 }
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,7 +8,13 @@ const { Path } = require("path-parser")
 const _ = require("lodash")
 const { URL } = require("url")
 const mongoose = require("mongoose")
-const Survey = require("../models/Survey")
+
+router.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+        recipients: false,
+    })
+    res.send(surveys)
+})
 
 router.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, body, subject, recipients } = req.body
@@ -34,20 +40,38 @@ router.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     }
 })
 
-router.get("/api/surveys/thanks", (req, res) => {
+router.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("thanks for answer")
 })
 
 router.post("/api/surveys/webhooks", (req, res) => {
-    const events = _.map(req.body, ({email}) => {
-        const pathname = new URL(event.url).pathname
-        const p = new Path("/api/surveys/:surveyId/:choice")
-        const match = p.test(pathname)
-        if(match){
-            return {surveyId:match.surveyId, choice:match.choice, email }
-        }
-    })
-    console.log(req.body)
+    const p = new Path("/api/surveys/:surveyId/:choice")
+    const events = _.chain(req.body)
+        .map(({ email, url }) => {
+            const match = p.test(new URL(url).pathname)
+            if (match) {
+                return { surveyId: match.surveyId, choice: match.choice, email }
+            }
+        })
+        .compact()
+        .uniqBy("email", "surveyId")
+        .each(({ surveyId, email, choice }) => {
+            Survey.updateOne(
+                {
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: { email: email, responded: false },
+                    },
+                },
+                {
+                    $inc: { [choice]: 1 },
+                    $set: { "recipients.$.responded": true },
+                    lastResponded: new Date(),
+                }
+            ).exec()
+        })
+        .value()
+
     res.send({})
 })
 
